1932a9b719a01a523b26cc9c19f0e5ef1d21d944
trabus322 <trabus322@trabus322.eu>
2024-03-19 10:59:54 +0100

    Initial user commit

---
+++ lab1/inc/song.h
@@ -0,0 +1,18 @@
+#ifndef SONG_H
+#define SONG_H
+
+#include <string>
+
+class Song {
+      private:
+    int id;
+
+    std::string name;
+    std::string artistName;
+
+      public:
+    Song();
+    Song(int id, std::string name, std::string artistName);
+};
+
+#endif
---
+++ lab1/src/song.cpp
@@ -0,0 +1,17 @@
+#include <string>
+
+#include "song.h"
+
+Song::Song() {
+    this->id = 0;
+
+    this->name = "";
+    this->artistName = "";
+}
+
+Song::Song(int id, std::string name, std::string artistName) {
+    this->id = id;
+
+    this->name = name;
+    this->artistName = artistName;
+}
